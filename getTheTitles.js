1bfff0a2f123e2af58658e4340e47363dae67c83
Brock <tammeusb@gmail.com>
2024-05-12 15:28:04 -0400

    Solve with iterative arr.forEach()

--- getTheTitles.js
+++ getTheTitles.js
@@ -1,6 +1,25 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    titles = [];
+    books.forEach((book) => {
+        titles = titles.concat(book.title);
+    })
+    return titles
 };
+
+const booksTest = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
+
+//iterate through 'books' array with books.forEach() and find each item.title,
+//arr.concat each title into a new array?
 
 // Do not edit below this line
 module.exports = getTheTitles;
